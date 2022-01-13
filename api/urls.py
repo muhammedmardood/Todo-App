f0d657ca8d8a4551bde631b249fd3dbb629d8f4c
muhammedmardood <mardoodmuhammed@gmail.com>
2022-01-13 20:04:10 +0300

    Second Commit

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('task-list/', taskList)
+    path('task-list/', taskList),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import *
 from .serializers import * 
 
 @api_view(["GET", "POST"])
 def taskList(request):
-    task = Task.objects.all()
-    serializer = TaskSerializer(task, many=True)
+    if request.method == "GET":
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
