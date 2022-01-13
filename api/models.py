682fb505395af4d4d589e37c93c4ac03232df1e5
muhammedmardood <mardoodmuhammed@gmail.com>
2022-01-13 23:48:23 +0300

    Completed the project

--- api/models.py
+++ api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
 class Task(models.Model):
     task = models.CharField(max_length=255)
     done = models.BooleanField(null=False, default=False)
+
+    class Meta:
+        ordering = ('-id',)
 
     def __str__(self):
         return self.task
