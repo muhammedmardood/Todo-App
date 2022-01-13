5f90560d58487656653099e6e372ccbc9a2b0d2e
muhammedmardood <mardoodmuhammed@gmail.com>
2022-01-13 21:13:02 +0300

    getting the rest_api finished

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -39,11 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # django libraries
     'rest_framework',
+    "corsheaders",
     # local apps
     'api.apps.ApiConfig',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
