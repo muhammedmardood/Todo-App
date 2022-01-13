06394329e5f53721259aa3e0433833eda3c2e3f8
muhammedmardood <mardoodmuhammed@gmail.com>
2022-01-13 20:36:11 +0300

    Completed the backend rest api

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('task-list/', taskList),
+    path('task/<int:pk>/', taskDetails),
+    path('task/delete/<int:pk>/', taskDelete)
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import *
-from .serializers import * 
+from .serializers import *
+
 
 @api_view(["GET", "POST"])
 def taskList(request):
@@ -18,3 +19,24 @@ def taskList(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+def taskDetails(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == "GET":
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = TaskSerializer(instance=task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response("item was deleted successfully")
